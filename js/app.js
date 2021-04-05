475c94f4eab6c62954424a8373b9bd6b75df0976
maryna1408 <cabanmarina9@gmail.com>
2021-04-05 12:07:47 +0300

    add plans-slider

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-const heroSlider = new Swiper(".slider");
+const plansSlider = new Swiper('.plans-slider', {
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+      },
+    }
+  })
